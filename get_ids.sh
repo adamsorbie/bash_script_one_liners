5091dea73d2e5140ffa2de200829103985ec0405
Adam <adam.sorbie@tum.de>
2019-02-18 17:12:48 +0100

    fixed extra line at top of file

--- get_ids.sh
+++ get_ids.sh
@@ -3,5 +3,5 @@ echo "enter filename: "
 read filename
 echo "enter output filename: "
 read output
-cut -f1 $filename | sed 's/^/>/' $output
+cut -f1 $filename | sed 's/^/>/' | tail -n +2 > $output
 
